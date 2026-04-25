c2fd703019baeef3e1786eda26c6ca99ad540b2e
= <=>
2026-04-25 21:20:53 +0800

    gradient

---
+++ docs/relay/tiled/gradient.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="gradient" tilewidth="1" tileheight="1" tilecount="32" columns="16" tilerendersize="grid">
+ <image source="gradient.png" width="16" height="2"/>
+</tileset>
--- docs/relay/tiled/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="tiles" tilewidth="64" tileheight="64" tilecount="200" columns="20">
- <image source="tiles.png" width="1280" height="640"/>
-</tileset>
