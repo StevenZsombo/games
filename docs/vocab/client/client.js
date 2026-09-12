46aa64d44a19d36c8b323dd88726f18bfd4d278d
= <=>
2026-09-12 16:25:14 +0800

    todo: vocab

---
+++ docs/vocab/client/client.js
@@ -0,0 +1,82 @@
+//#region univ
+var univ = {
+    isOnline: true, //server is offline!
+    PORT: 80,
+    framerateUnlocked: false,
+    dtUpperLimit: 1000 / 15,//1000 / 30,
+    denybuttons: false,
+    showFramerate: false,
+    imageSmoothingEnabled: true,
+    imageSmoothingQuality: "high", // options: "low", "medium", "high"
+    canvasStyleImageRendering: "auto",
+    //BROKEN
+    fontFile: null, // "resources/victoriabold.png" //set to null otherwise
+    //BROKEN
+    filesList: "", //space-separated
+    on_each_start: null,
+    on_first_run: null,
+    on_first_run_blocking: null,
+    on_first_run_async: null, //async function. overrides on_first_run_blocking
+    on_next_game_once: null,
+    on_beforeunload: null,
+    allowQuietReload: false,
+    acquireNameStr: "Your English name (at least 4 letters):", //for chat
+    acquireNameMoreStr: "(English name + homeroom)" //for Supabase
+}
+//#endregion
+
+
+class Game extends GameShared {
+    //#region initialize_more
+    initialize_more() { }
+    async initialize_async() {
+
+
+
+    }
+    //#endregion
+
+    //#region update_more
+    update_more(dt) {
+
+
+
+
+
+
+    }
+    //#endregion
+
+
+    //#region draw_more
+    draw_more(screen) {
+
+
+
+
+
+
+    }
+    //#endregion
+
+    //#region next_loop_more
+    next_loop_more() {
+
+
+
+
+    }//#endregion
+
+
+
+    //
+} //this is the last closing brace for class Game
+
+
+
+//#region dev options
+/// dev options
+const dev = {
+
+
+}/// end of dev
---
+++ docs/vocab/shared/gameShared.js
@@ -0,0 +1,7 @@
+class GameShared extends GameCore {
+
+    
+
+
+   
+}
