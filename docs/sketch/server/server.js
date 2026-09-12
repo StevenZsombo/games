6187e9594caef3329b4a0c75e2e4432af4b098cc
= <=>
2026-09-12 13:31:33 +0800

    made sketch (i think)

---
+++ docs/sketch/server/server.js
@@ -0,0 +1,128 @@
+const listener = new Listener()
+chat = listener.chat
+let disconnectedIndicator = null
+chat.on_disconnect = () => disconnectedIndicator ??= GameEffects.popup("LOST CONNECTION...", { floatTime: Infinity, posFrac: [.5, .2], moreButtonSettings: { color: "red" } })
+chat.on_join = () => { disconnectedIndicator?.close(); disconnectedIndicator = null }
+Chat.defaultWeeInterval = 250
+Chat.defaultWeeRetries = 5
+Chat.defaultSpamRetries = 1000
+
+const ob = new Observatory()
+const em = new EventManager()
+const EM = {}
+const bpop = str => GameEffects.popup(str, GameEffects.popupPRESETS.sideError)
+const sessionID = MM.randomID() + MM.randomID()
+
+
+class Person extends Participant {
+    initialize() {
+
+    }
+
+}
+
+class Game extends GameShared {
+    //#region initialize_more
+    initialize_more() { }
+    async initialize_async() {
+
+
+
+    }
+    //#endregion
+
+
+
+    ///end initialize_more^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    ///                                         ^^^^INITIALIZE^^^^                                                   ///
+    ///                                                                                                              ///
+    ///                                               UPDATE                                                         ///
+    /// start update_more:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    //#region update_more
+
+    update_more(dt) {
+
+
+
+
+    }
+
+    //#endregion
+    ///end update_more^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    ///                                           ^^^^UPDATE^^^^                                                     ///
+    ///                                                                                                              ///
+    ///                                                DRAW                                                          ///
+    ///start update_more::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    //#region draw_more
+
+    draw_more(screen) {
+
+
+
+
+
+
+    }
+    //#endregion
+    ///end draw_more^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    ///                                            ^^^^DRAW^^^^                                                      ///
+    ///                                                                                                              ///
+    ///                                              NEXT_LOOP                                                       ///
+    ///start next_loop_more:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    //#region next_loop_more
+    next_loop_more() {
+
+
+
+
+    }//#endregion
+    ///end next_loop_more^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    ///                                          ^^^^NEXT_LOOP^^^^                                                   ///
+    ///                                                                                                              ///
+    ///                                                                                                              ///
+    /// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+} //this is the last closing brace for class Game
+
+
+
+
+
+
+
+//#region univ
+var univ = {
+    isOnline: false, //server is offline!
+    PORT: 80,
+    framerateUnlocked: false,
+    dtUpperLimit: 1000 / 15,//1000 / 30,
+    denybuttons: false,
+    showFramerate: false,
+    imageSmoothingEnabled: true,
+    imageSmoothingQuality: "high", // options: "low", "medium", "high"
+    canvasStyleImageRendering: "auto", //options: "auto", "smooth", "crisp-edges", "pixelated"
+    //BROKEN
+    fontFile: null, // "resources/victoriabold.png" //set to null otherwise
+    //BROKEN
+    filesList: "", //space-separated
+    on_each_start: null,
+    on_first_run: null,
+    on_first_run_blocking: null,
+    on_first_run_async: null,
+    //async function. overrides on_first_run_blocking
+    on_next_game_once: null,
+    on_beforeunload: null,
+    allowQuietReload: true,
+    acquireNameStr: "Your English name (at least 4 letters):", //for chat
+    acquireNameMoreStr: "(English name + homeroom)" //for Supabase
+}
+//#endregion
+
+
+//#region dev options
+/// dev options dev.dev.dev.
+const dev = {
+}/// end of dev
